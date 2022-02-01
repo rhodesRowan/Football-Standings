08b86fbebe576b041894778613ffba1aabfdf2b7
Rowan Rhodes <rowandrhodes@gmail.com>
2022-02-01 06:53:48 +0000

    Refactor model logic

--- football-standings/Models/Statistic.swift
+++ football-standings/Models/Statistic.swift
@@ -23,3 +23,69 @@ struct Statistic: Codable {
         case abbreviation, value, displayValue, id, summary
     }
 }
+
+enum StaticsticAbbreviation: String, Codable {
+    case a = "A"
+    case d = "D"
+    case f = "F"
+    case gd = "GD"
+    case gp = "GP"
+    case l = "L"
+    case p = "P"
+    case pd = "PD"
+    case ppg = "PPG"
+    case r = "R"
+    case rc = "RC"
+    case total = "Total"
+    case w = "W"
+}
+
+enum StatisticDescription: String, Codable {
+    case draws = "Draws"
+    case gamesPlayed = "Games Played"
+    case goalDifference = "Goal Difference"
+    case goalsAgainst = "Goals Against"
+    case goalsFor = "Goals For"
+    case losses = "Losses"
+    case overall = "Overall"
+    case overallRecord = "Overall Record"
+    case pointDeductions = "Point Deductions"
+    case points = "Points"
+    case pointsPerGame = "Points Per Game"
+    case rank = "Rank"
+    case rankChange = "Rank Change"
+    case wins = "Wins"
+}
+
+enum Name: String, Codable {
+    case allSplits = "All Splits"
+    case deductions = "deductions"
+    case gamesPlayed = "gamesPlayed"
+    case losses = "losses"
+    case pointDifferential = "pointDifferential"
+    case points = "points"
+    case pointsAgainst = "pointsAgainst"
+    case pointsFor = "pointsFor"
+    case ppg = "ppg"
+    case rank = "rank"
+    case rankChange = "rankChange"
+    case ties = "ties"
+    case wins = "wins"
+}
+
+enum ShortDisplayName: String, Codable {
+    case a = "A"
+    case d = "D"
+    case deductions = "Deductions"
+    case f = "F"
+    case gd = "GD"
+    case gp = "GP"
+    case l = "L"
+    case over = "OVER"
+    case p = "P"
+    case ppg = "PPG"
+    case rank = "Rank"
+    case rankChange = "Rank Change"
+    case w = "W"
+}
+
